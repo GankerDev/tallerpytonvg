fde80bd0b2671e2305d76e35bb2d5aeaf2c1a234
GankerDev <joshua.cid@virginiogomez.cl>
2018-03-23 02:46:01 -0300

    Agregamos APP boletin

--- src/python1/settings.py
+++ src/python1/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'boletin.apps.BoletinConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
